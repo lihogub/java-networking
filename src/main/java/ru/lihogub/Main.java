8b9521039ba9079fe26ff8c3578c26b5bd9c59f5
Lihogub Oleg <oligli@yandex.ru>
2021-09-02 20:53:55 +0300

    rx impl

--- src/main/java/ru/lihogub/Main.java
+++ src/main/java/ru/lihogub/Main.java
@@ -1,23 +1,22 @@
 package ru.lihogub;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Lol");
-        start();
-    }
-
-    public static void start() throws IOException {
         ServerSocket serverSocket = new ServerSocket(1234);
         Socket socket = serverSocket.accept();
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String greeting = in.readLine();
-        System.out.println(greeting);
+        Client client = new Client(socket);
+        client
+                .observeMsg()
+                .doOnEach(System.out::println)
+                .subscribe(it -> {
+                    client
+                            .sendMsg(it)
+                            .subscribe();
+                });
     }
 }
